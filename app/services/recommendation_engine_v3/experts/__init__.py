87b46e866bfb9f2f940fa71b0c17f977b7e71357
Mohan Ganesh <mohanganesh3@users.noreply.github.com>
2025-12-10 18:03:18 +0530

    Fix: Export expert classes from experts/__init__.py (NutritionExpert, MovementExpert, MindfulnessExpert)

--- app/services/recommendation_engine_v3/experts/__init__.py
+++ app/services/recommendation_engine_v3/experts/__init__.py
@@ -1,3 +1,15 @@
 """
 Expert modules for Recommendation Engine V3
 """
+
+from .base_expert import BaseExpert
+from .nutrition_expert import NutritionExpert
+from .movement_expert import MovementExpert
+from .mindfulness_expert import MindfulnessExpert
+
+__all__ = [
+    'BaseExpert',
+    'NutritionExpert',
+    'MovementExpert',
+    'MindfulnessExpert'
+]
