d894926b8184364c0a87d0f7307b461561a49303
Mohan Ganesh <mohanganesh3@users.noreply.github.com>
2026-01-05 15:11:35 +0530

    Shorten merge revision id to fit alembic_version

--- alembic/versions/merge_actionable_insights_and_symptom_threads.py
+++ alembic/versions/merge_actionable_insights_and_symptom_threads.py
@@ -1,6 +1,10 @@
 """Merge actionable_insights and symptom check-in threads heads.
 
-Revision ID: merge_actionable_insights_and_symptom_threads
+NOTE: This revision id must remain <= 32 characters because the production
+`alembic_version.version_num` column is VARCHAR(32). A longer revision id will
+cause deployment-time migrations to fail when Alembic updates that table.
+
+Revision ID: merge_action_ins_symptom
 Revises: add_actionable_insights, add_symptom_checkin_threads
 Create Date: 2026-01-05
 
@@ -11,7 +15,7 @@ Create Date: 2026-01-05
 from alembic import op  # noqa: F401
 
 # revision identifiers, used by Alembic.
-revision = "merge_actionable_insights_and_symptom_threads"
+revision = "merge_action_ins_symptom"
 down_revision = ("add_actionable_insights", "add_symptom_checkin_threads")
 branch_labels = None
 depends_on = None
